69d98c9fd8475bde77f1971f576abebdb4c06d04
Elliott Landsborough <elliott.landsborough@gmail.com>
2012-02-08 20:19:29 +0000

    wonderful

---
+++ codeigniter/application/views/user_loginform.php
@@ -0,0 +1,25 @@
+<?php
+$this->load->helper('form');
+echo validation_errors();
+$attributes = array('class' => 'login');
+echo form_open('user/login', $attributes);
+$username = array(
+              'name'        => 'username',
+              'class'          => 'username',
+              'value'       => '',
+              'maxlength'   => '26',
+              'size'        => '26',
+            );
+echo form_input($username);
+$password = array(
+              'name'        => 'password',
+              'class'          => 'password',
+              'value'       => '',
+              'maxlength'   => '32',
+              'size'        => '26',
+              //'type'        => 'password',
+            );
+echo form_input($password);
+echo form_submit('dologon', 'login');
+form_close()
+?>
---
+++ codeigniter/application/views/user_regform.php
@@ -0,0 +1,33 @@
+<?php
+//echo validation_errors();
+$this->load->helper('form');
+$attributes = array('class' => 'register');
+echo form_open('user/register', $attributes);
+$username = array(
+              'name'        => 'username',
+              'class'          => 'username',
+              'value'       => 'username',
+              'maxlength'   => '26',
+              'size'        => '16',
+            );
+$email = array(
+              'name'        => 'email',
+              'class'          => 'email',
+              'value'       => 'email address',
+              'maxlength'   => '40',
+              'size'        => '16',
+            );
+$password = array(
+              'name'        => 'password',
+              'class'          => 'password',
+              'value'       => 'password',
+              'maxlength'   => '32',
+              'size'        => '16',
+              //'type'		=> 'password',
+            );
+echo form_input($username);
+echo form_input($email);
+echo form_input($password);
+echo form_submit('doregister', 'register');
+form_close()
+?>
