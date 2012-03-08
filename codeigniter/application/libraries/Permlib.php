7b2d27dd1b96798164319c4ac24faa852f8ed7ff
elliott <elliott@landsborough.co.uk>
2012-03-08 12:47:52 +0000

    wonderful

--- codeigniter/application/libraries/Permlib.php
+++ codeigniter/application/libraries/Permlib.php
@@ -8,105 +8,92 @@ class Permlib {
 	ONLY ADD NEW ONES - DO NOT MODIFY OLD ONES.
 	*/
 	// maybe don't have these in consts and have them in an array instead??
-	const VIEW_PROFILE = 1; // equivalent of being banned
-    const EDIT_PROFILE = 2; // in case we want to disable edits for specific users
-    const DISABLE_PROFILE = 4; // user can disable OWN profile only
-    const VIEW_VOUCHER = 8;
-    const EDIT_VOUCHER = 16; // can edit vouchers
-    const DISABLE_VOUCHER = 32; // disable OWN vouchers only
-    const GLOBAL_EDIT = 64; // can edit all
-    const GLOBAL_DISABLE = 128; // can disable all
-    const SUPERADMIN = 256; // everything including view ALL disabled profiles and vouchers
+	const ACCESS = 1; // equivalent of being unbanned
+    const EDIT_SELF = 2; // in case we want to disable edits for specific users
+    const DISABLE_SELF = 4; // user can disable OWN profile only
+    const VIEW_VOUCHERS = 8;
+    const OWN_VOUCHERS = 16; // can own/edit vouchers
+    const GLOBAL_VOUCHERS = 32; // can edit/disable ALL vouchers
+    const GLOBAL_VIEW = 64; // can view anything
+    const GLOBAL_EDIT = 128; // can edit anything
+    const GLOBAL_DISABLE = 256; // can disable anything
+    const SUPERADMIN = 512; // can add/remove global operators
     
     public function __construct()
     {
     	$Mask = 0;
         $this->Mask = $Mask;
+        $CI =& get_instance();
+        $dbperms = $this->getperms($CI->session->userdata('user_id'));
+        if ($dbperms != $CI->session->userdata('user_perms'));
+        {
+        	$CI->session->set_userdata('user_perms', $dbperms);
+        }
     }
 
-	// return the users permissions
-	public function getperms($user_id=null)
+    // return the users permissions
+    public function getperms($user_id=null)
 	{
-		$CI =& get_instance();
+		$CI =& get_instance();	
 		if ($user_id==null)
 		{
-			// is it safe to store user_id in php session? if not then uncomment this shit
-			// which will add an extra query per getperms() but will keep user_id out of php's session
-			/*$login = $CI->session->userdata('user_login');
-			$email = $CI->session->userdata('user_email');
-        	$query = $CI->db->get_where('users', array('user_name' => $login,'user_email' => $email));
-        	$row = $query->row(0);
-        	$input['user_id'] = $row->user_id;*/
-        	$input['user_id'] = $CI->session->userdata('user_id');
-        }
-        else
-        {
-        	$input['user_id'] = $user_id;
-        }
-        $pquery = $CI->db->get_where('user_perms', array('user_id' => $input['user_id']));
-        if ($pquery->num_rows() == 0)
-        {
-        	if ($this->setperms($input))
-        	{
-        		$perms = $this->getperms($input['user_id']);
-        	}
-        }
-        else
-        {
-        	$prow = $pquery->row(0);
-        	$perms = $prow->user_perms;
-        }
-        return $perms;
-	}
-
-	// change a users permissions
-	/* usage:
-	$newperms = array( 
-		permlib::VIEW_PROFILE,
-		permlib::EDIT_PROFILE,
-		permlib::DISABLE_PROFILE,
-		permlib::VIEW_VOUCHER
-		);
-
-	$array = array(
-		'user_id' => $user_id,
-		'newperms'=> $newperms
-		);
-	setperms($array);
-	*/
-	public function setperms($input=null)
-	{
-		$CI =& get_instance();
-		$perms->user_id = $input['user_id'];
-		if (!isset($input['newperms']))
-		{
-			// default permissions for standard users
-			$permsarray = array( 
-				permlib::VIEW_PROFILE,
-				permlib::EDIT_PROFILE,
-				permlib::DISABLE_PROFILE,
-				permlib::VIEW_VOUCHER
-				);
+			$input['user_id'] = $CI->session->userdata('user_id');
 		}
 		else
 		{
-			//$this->Mask = $this->getperms($input['user_id']);
-			$this->Mask = 0;
-			$permsarray = $input['newperms'];
+			$input['user_id'] = $user_id;
 		}
+		$pquery = $CI->db->get_where('user_perms', array('user_id' => $input['user_id']));
+		if ($pquery->num_rows() == 0)
+		{
+			if ($this->DefaultPerms($input))
+        	{
+        		$pquery = $CI->db->get_where('user_perms', array('user_id' => $input['user_id']));
+        	}
+		}
+		$prow = $pquery->row(0);
+       	$perms = $prow->user_perms;
+        return $perms;
+	}
+
+	// setup default permission set
+	public function DefaultPerms($input)
+	{
+		$CI =& get_instance();
+		$perms->user_id = $input['user_id'];
+		// default permissions for standard users
+		$permsarray = array( 
+			//permlib::ACCESS,
+			permlib::EDIT_SELF,
+			permlib::DISABLE_SELF,
+			permlib::VIEW_VOUCHERS
+			);
 		foreach ($permsarray as $perm)
 		{
 			$this->AddPermission($perm);
 		}
-		// in here goes the rest of function - basically need to work out whether to add or remove permissions instead of always adding them.
 		$perms->user_perms = $this->GetMask();
-		if (!isset($input['newperms']))
+		return ($CI->db->insert('user_perms', $perms));
+	}
+
+	public function SetPermissions($input)
+	{
+		$CI =& get_instance();
+		$user_id = $input['user_id'];
+		$perms->user_perms = $input['user_perms'];
+		$CI->db->where('user_id', $user_id);
+		return $CI->db->update('user_perms', $perms);
+	}
+
+	public function SetMask($input)
+	{
+		if ($input['newmask']==null)
 		{
-			return ($CI->db->insert('user_perms', $perms));
+			$this->Mask = $this->getperms($input['user_id']);
 		}
 		else
 		{
-			return ($CI->db->update('user_perms', $perms));
+			$this->Mask = $input['newmask'];
 		}
 	}
 
@@ -130,12 +117,6 @@ class Permlib {
          return $this->Mask;
     }
 
-    /*
-	if($this->permlib->InvokePermission(permlib::VIEW_PROFILE))
-	{
-	    //User can vew profiles
-	}
-    */
 }
 
 ?>
