d421c0e8ffb37905b5b21e9ebdd6becece5a3bba
elliott <elliott@landsborough.co.uk>
2012-03-08 20:57:17 +0000

    wonderful

--- codeigniter/application/libraries/Permlib.php
+++ codeigniter/application/libraries/Permlib.php
@@ -3,22 +3,51 @@
 class Permlib {
 
 	/**
-	permissions - must increase in squares to work
+	permissions - must increase in doubles to work
 	AFTER DEPLOYMENT THESE CANNOT BE CHANGED EVER WITHOUT SERIOUS RISK OF CLUSTERFUCK.
 	ONLY ADD NEW ONES - DO NOT MODIFY OLD ONES.
+	see comments at bottom for numbers
+	remember 32/64bit compatibility issues - ideally have no more than 31 different perm types else shit will hit fans
 	*/
 	// maybe don't have these in consts and have them in an array instead??
-	const ACCESS = 1; // equivalent of being unbanned
-    const EDIT_SELF = 2; // in case we want to disable edits for specific users
-    const DISABLE_SELF = 4; // user can disable OWN profile only
-    const VIEW_VOUCHERS = 8;
-    const OWN_VOUCHERS = 16; // can own/edit vouchers
-    const GLOBAL_VOUCHERS = 32; // can edit/disable ALL vouchers
-    const GLOBAL_VIEW = 64; // can view anything
-    const GLOBAL_EDIT = 128; // can edit anything
-    const GLOBAL_DISABLE = 256; // can disable anything
-    const SUPERADMIN = 512; // can add/remove global operators
-    
+	const ACCESS = 1; // email validation complete - use this to ban people
+	const COMMUNICATE = 2; // communication
+    const EDIT = 4; // profile edit
+    const VOUCHERS = 8; // vouchers
+    const BRAND = 16; // brand profile
+    const MODERATOR = 32; // mod profile
+    const ADMIN = 64; // admin profile
+    // insert categorized perms here - up to 32 per cat
+    /*
+	communicate:
+		post on walls? (yes/no) visibility:
+			public
+			friends only
+			private
+		update status? (yes/no) visibility:
+			public
+			friends only
+			private
+		can have friends? (yes/no) visibility:
+			public
+			friends only
+			private
+		photo (yes/no) visibility:
+			public
+			friends only
+			private
+		chat (yes/no)
+		private message (yes/no)
+
+	edit (yes/no)
+
+	vouchers:
+		own (yes/no)
+		trade (yes/no)
+		bid?? (yes/no)
+
+    */
+
     public function __construct()
     {
     	$Mask = 0;
@@ -66,9 +95,9 @@ class Permlib {
 		$perms->user_id = $input['user_id'];
 		// default permissions for standard users
 		$permsarray = array( 
-			permlib::EDIT_SELF,
-			permlib::DISABLE_SELF,
-			permlib::VIEW_VOUCHERS
+			permlib::COMMUNICATE,
+			permlib::EDIT,
+			permlib::VOUCHERS
 			);
 		foreach ($permsarray as $perm)
 		{
@@ -124,4 +153,84 @@ class Permlib {
 
 }
 
+	/* permissions numbers:
+	1. 1
+	2. 2
+	3. 4
+	4. 8
+	5. 16
+	6. 32
+	7. 64
+	8. 128
+	9. 256
+	10. 512
+	11. 1024
+	12. 2048
+	13. 4096
+	14. 8192
+	15. 16384
+	16. 32768
+	17. 65536
+	18. 131072
+	19. 262144
+	20. 524288
+	21. 1048576
+	22. 2097152
+	23. 4194304
+	24. 8388608
+	25. 16777216
+	26. 33554432
+	27. 67108864
+	28. 134217728
+	29. 268435456
+	30. 536870912
+	31. 1073741824
+	32. 2147483648
+	// stop here if 32bit os
+	33. 4294967296
+	34. 8589934592
+	35. 17179869184
+	36. 34359738368
+	37. 68719476736
+	38. 137438953472
+	39. 274877906944
+	40. 549755813888
+	41. 1099511627776
+	42. 2199023255552
+	43. 4398046511104
+	44. 8796093022208
+	45. 17592186044416
+	46. 35184372088832
+	47. 70368744177664
+	48. 140737488355328
+	49. 281474976710656
+	50. 562949953421312
+	51. 1125899906842624
+	52. 2251799813685248
+	53. 4503599627370496
+	54. 9007199254740992
+	55. 18014398509481984
+	56. 36028797018963968
+	57. 72057594037927936
+	58. 144115188075855872
+	59. 288230376151711744
+	60. 576460752303423488
+	61. 1152921504606846976
+	62. 2305843009213693952
+	63. 4611686018427387904
+	*/
+	/*
+	public function profileview($query)
+	{
+		$row = $query->row(0);
+		echo $row->user_name;
+		$j=0;
+		for ($i=1; $j<63;)
+ 		{
+ 			$j++;
+  			echo $j.". ".$i."<br />";
+  			$i = $i*2;
+  		}
+	}
+	*/
 ?>
